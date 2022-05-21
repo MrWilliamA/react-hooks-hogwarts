31bd3ebf8d5a61eb341107d4212ce091de001c5e
MrWilliamA <will.archer@hotmail.com>
2022-05-21 10:04:30 +1000

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,41 @@
-import React from "react";
-import hogs from "../porkers_data.js";
-import Hoglist from "./Hoglist.js";
+import React, { useState } from "react";
+import hogs from "../porkers_data";
+import Filter from "./Filter";
+import HogList from "./HogList";
 import Nav from "./Nav";
 
 function App() {
-  console.log(hogs);
+  const [showGreased, setShowGreased] = useState(false);
+  const [sortBy, setSortBy] = useState("name");
+
+  const hogsToDisplay = hogs
+    .filter((hog) => (showGreased ? hog.greased : true))
+    .sort((hog1, hog2) => {
+      if (sortBy === "weight") {
+        return hog1.weight - hog2.weight;
+      } else {
+        return hog1.name.localeCompare(hog2.name);
+      }
+    });
+
   return (
-    <div className="App">
-      <Nav />
-      <Hoglist hogs={hogs} />
+    <div className="ui grid container App">
+      <div className="sixteen wide column centered">
+        <Nav />
+      </div>
+      <div className="sixteen wide column centered">
+        <Filter
+          showGreased={showGreased}
+          onChangeShowGreased={setShowGreased}
+          sortBy={sortBy}
+          onChangeSortBy={setSortBy}
+        />
+      </div>
+      <div className="sixteen wide column centered">
+        <HogList hogs={hogsToDisplay} />
+      </div>
     </div>
   );
 }
 
 export default App;
-
-// single hog component
-// HogList Component filtering each hog with select form, displays name and image
-//  chain the filter so multiple selections can be made
-
-// hidden items for current hog only
